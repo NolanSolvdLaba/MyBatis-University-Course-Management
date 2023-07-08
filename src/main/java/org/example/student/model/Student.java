3543a4b75430cf1d4d0ba8082870e6325c5716c7
NoleyNarbar <nolan.geiger@gmail.com>
2023-07-08 13:16:58 -0400

    Implemented the StudentMapper, StudentService and just the StudentModel.

--- src/main/java/org/example/student/model/Student.java
+++ src/main/java/org/example/student/model/Student.java
@@ -1,4 +1,60 @@
 package org.example.student.model;
 
 public class Student {
+    private int studentId;
+    private String studentName;
+    private int admissionYear;
+    private int departmentId;
+    private String departmentName;
+
+    public Student() {
+    }
+
+    public Student(int studentId, String studentName, int admissionYear, int departmentId, String departmentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.admissionYear = admissionYear;
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getAdmissionYear() {
+        return admissionYear;
+    }
+
+    public void setAdmissionYear(int admissionYear) {
+        this.admissionYear = admissionYear;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
 }
--- src/main/java/org/example/student/service/StudentService.java
+++ src/main/java/org/example/student/service/StudentService.java
@@ -1,4 +1,29 @@
 package org.example.student.service;
 
+import org.example.student.mapper.StudentMapper;
+import org.example.student.model.Student;
+
 public class StudentService {
+    private final StudentMapper studentMapper;
+
+    public StudentService(StudentMapper studentMapper) {
+        this.studentMapper = studentMapper;
+    }
+
+    public Student getStudentById(int studentId) {
+        return studentMapper.getStudentById(studentId);
+    }
+
+    public void insertStudent(Student student) {
+        studentMapper.insertStudent(student);
+    }
+
+    public void updateStudent(Student student) {
+        studentMapper.updateStudent(student);
+    }
+
+    public void deleteStudent(int studentId) {
+        studentMapper.deleteStudent(studentId);
+    }
+
 }
